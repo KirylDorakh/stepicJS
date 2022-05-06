8a64e340e8d0302e28d3a73fa36dc7c3b3217a7f
Kiryl Dorakh <kirill.dorokh@gmail.com>
2022-05-06 13:03:52 +0400

    JS

---
+++ main.js
@@ -0,0 +1,22 @@
+var x = 1;
+let studentName;
+
+// comment
+alert('Hello world');
+
+/*
+Big comment
+ */
+
+x = 22
+a = 5
+
+var myName = "Ivan";
+alert(typeof myName);
+
+function testA(a){
+    var x = a;
+    return a;
+}
+
+alert(testA(10))
